fe8348712ddd334c1a7221cac6a7d5e87cdac2d9
FATALERRORRACE <frdrummer@gmail.com>
2025-01-20 15:41:46 -0500

    lastest changes

--- app/Services/XlssExport.php
+++ app/Services/XlssExport.php
@@ -303,7 +303,7 @@ class XlssExport
             ->first();
 
         $consolidado['Inventariado'] = DB::table($tableName)
-            ->select('master.Proceso' ,'master.Localizacion',  DB::raw("COUNT($tableName.id) as total"), DB::raw("SUM(master.Precio) as totalprice"))
+            ->select('master.Proceso', 'master.Localizacion',  DB::raw("COUNT($tableName.id) as total"), DB::raw("SUM(master.Precio) as totalprice"))
             ->join('master', 'master.C_Barras', '=', "{$tableName}.C_Barras")
             ->where("{$tableName}.Estado", '<>', '')
             ->groupBy('master.Localizacion', 'master.Proceso')
@@ -317,10 +317,9 @@ class XlssExport
             ->first();
 
         $consolidado['Faltante'] = DB::table($tableName)
-            ->select('master.Proceso' ,'master.Localizacion', DB::raw("COUNT($tableName.id) as total"), DB::raw("SUM(master.Precio) as totalprice"))
+            ->select('master.Proceso', 'master.Localizacion', DB::raw("COUNT($tableName.id) as total"), DB::raw("SUM(master.Precio) as totalprice"))
             ->join('master', 'master.C_Barras', '=', "{$tableName}.C_Barras")
-            ->where("{$tableName}.Estado", '<>', 'I')
-            ->where("{$tableName}.Estado", '<>', 'P')
+            ->where("{$tableName}.Estado", '')
             ->whereNotIn('master.Proceso', ['Nivel Central', 'Material en tránsito'])
             ->groupBy('master.Localizacion', 'master.Proceso')
             ->orderBy('master.Localizacion')
@@ -329,24 +328,21 @@ class XlssExport
         $consolidado['FaltanteSuma'] = DB::table($tableName)
             ->select(DB::raw("COUNT($tableName.id) as total"), DB::raw("SUM(master.Precio) as totalprice"))
             ->join('master', 'master.C_Barras', '=', "{$tableName}.C_Barras")
-            ->where("{$tableName}.Estado", '<>', 'I')
-            ->where("{$tableName}.Estado", '<>', 'P')
+            ->where("{$tableName}.Estado", '')
             ->whereNotIn('master.Proceso', ['Nivel Central', 'Material en tránsito'])
             ->first();
 
         $consolidado['FaltanteNivelCentral'] = DB::table($tableName)
             ->select(DB::raw("COUNT($tableName.id) as total"), DB::raw("SUM(master.Precio) as totalprice"))
             ->join('master', 'master.C_Barras', '=', "{$tableName}.C_Barras")
-            ->where("{$tableName}.Estado", '<>', 'I')
-            ->where("{$tableName}.Estado", '<>', 'P')
+            ->where("{$tableName}.Estado", '')
             ->where('master.Proceso', 'Nivel Central')
             ->first();
 
         $consolidado['FaltanteTransito'] = DB::table($tableName)
             ->select(DB::raw("COUNT($tableName.id) as total"), DB::raw("SUM(master.Precio) as totalprice"))
             ->join('master', 'master.C_Barras', '=', "{$tableName}.C_Barras")
-            ->where("{$tableName}.Estado", '<>', 'I')
-            ->where("{$tableName}.Estado", '<>', 'P')
+            ->where("{$tableName}.Estado", '')
             ->where('master.Proceso', 'Material en tránsito')
             ->first();
 
@@ -414,66 +410,32 @@ class XlssExport
         $spreadsheet->getActiveSheet()->getColumnDimension('D')->setWidth(16);
         $spreadsheet->getActiveSheet()->getColumnDimension('E')->setWidth(16);
         $spreadsheet->getActiveSheet()->getColumnDimension('F')->setWidth(16);
-        $spreadsheet->getActiveSheet()->getStyle("A1:F1")->getFont()->setBold(true);
-        $spreadsheet->getActiveSheet()->getStyle('A1:F1')->getFill()->setFillType(\PhpOffice\PhpSpreadsheet\Style\Fill::FILL_SOLID)->getStartColor()->setARGB('DDE5ED');
-        $spreadsheet->getActiveSheet()->setCellValue('A1', 'INFORME CONSOLIDADO DE TOMA FÍSICA');
-        $spreadsheet->getActiveSheet()->mergeCells('A1:F1', \PhpOffice\PhpSpreadsheet\Worksheet\Worksheet::MERGE_CELL_CONTENT_MERGE);
-        $this->addHeaderStyle($spreadsheet, 'A1:F1');
 
-        $spreadsheet->getActiveSheet()->getStyle("A3:D6")->applyFromArray([
-            'borders' => [
-                'allBorders' => [
-                    'borderStyle' => \PhpOffice\PhpSpreadsheet\Style\Border::BORDER_THIN,
-                    'color' => ['argb' => '000000'],
-                ],
-            ],
-        ]);
+        $spreadsheet->getActiveSheet()->setCellValue('A1', 'INFORME CONSOLIDADO DEL PROCESO DE INVENTARIO EN PERGAMUM');
+        $spreadsheet->getActiveSheet()->mergeCells('A1:D1', \PhpOffice\PhpSpreadsheet\Worksheet\Worksheet::MERGE_CELL_CONTENT_MERGE);
+        $this->addHeaderStyle($spreadsheet, 'A1:D1');
+
         $spreadsheet->getActiveSheet()->setCellValue('A3', 'UBICACIÓN');
         $spreadsheet->getActiveSheet()->mergeCells('A3:A4', \PhpOffice\PhpSpreadsheet\Worksheet\Worksheet::MERGE_CELL_CONTENT_MERGE);
         $this->addHeaderStyle($spreadsheet, 'A3:A4');
-        $spreadsheet->getActiveSheet()->setCellValue('B3', 'TOMA FíSICA');
-        $spreadsheet->getActiveSheet()->mergeCells('B3:D3', \PhpOffice\PhpSpreadsheet\Worksheet\Worksheet::MERGE_CELL_CONTENT_MERGE);
-        $this->addHeaderStyle($spreadsheet, 'B3:D3');
-
-        $spreadsheet->getActiveSheet()->setCellValue('B4', 'CAPTURADOS');
-        $spreadsheet->getActiveSheet()->setCellValue('C4', 'REPETIDOS');
-        $spreadsheet->getActiveSheet()->setCellValue('D4', 'TOTAL');
-        $this->addHeaderStyle($spreadsheet, 'B4:D4');
-        $spreadsheet->getActiveSheet()->setCellValue('A5', $table['Nombre']);
-        $spreadsheet->getActiveSheet()->setCellValue('B5', '10.000');
-        $spreadsheet->getActiveSheet()->setCellValue('C5', '10.000');
-        $spreadsheet->getActiveSheet()->setCellValue('D5', '10.000');
-
-        $spreadsheet->getActiveSheet()->setCellValue('A6', 'TOTALES');
-        $spreadsheet->getActiveSheet()->setCellValue('B6', '10.000');
-        $spreadsheet->getActiveSheet()->setCellValue('C6', '10.000');
-        $spreadsheet->getActiveSheet()->setCellValue('D6', '10.000');
-
-        $spreadsheet->getActiveSheet()->setCellValue('A8', 'INFORME CONSOLIDADO DEL PROCESO DE INVENTARIO EN PERGAMUM');
-        $spreadsheet->getActiveSheet()->mergeCells('A8:D8', \PhpOffice\PhpSpreadsheet\Worksheet\Worksheet::MERGE_CELL_CONTENT_MERGE);
-        $this->addHeaderStyle($spreadsheet, 'A8:D8');
-
-        $spreadsheet->getActiveSheet()->setCellValue('A10', 'UBICACIÓN');
-        $spreadsheet->getActiveSheet()->mergeCells('A10:A11', \PhpOffice\PhpSpreadsheet\Worksheet\Worksheet::MERGE_CELL_CONTENT_MERGE);
-        $this->addHeaderStyle($spreadsheet, 'A10:A11');
 
 
-        $spreadsheet->getActiveSheet()->setCellValue('B10', 'PROCESO DE INVENTARIO');
-        $this->addHeaderStyle($spreadsheet, 'B10');
-        $spreadsheet->getActiveSheet()->setCellValue('B11', 'INVENTARIADOS');
-        $this->addHeaderStyle($spreadsheet, "B11");
+        $spreadsheet->getActiveSheet()->setCellValue('B3', 'PROCESO DE INVENTARIO');
+        $this->addHeaderStyle($spreadsheet, 'B3');
+        $spreadsheet->getActiveSheet()->setCellValue('B4', 'INVENTARIADOS');
+        $this->addHeaderStyle($spreadsheet, "B4");
         $sumtotal = 0;
         foreach ($dataUbicaciones['ubicaciones'] as $key => $value) {
-            $spreadsheet->getActiveSheet()->setCellValue("A" . ($key + 12), $value->Localizacion);
-            $spreadsheet->getActiveSheet()->setCellValue("B" . ($key + 12), $value->total);
+            $spreadsheet->getActiveSheet()->setCellValue("A" . ($key + 5), $value->Localizacion);
+            $spreadsheet->getActiveSheet()->setCellValue("B" . ($key + 5), $value->total);
             $sumtotal += $value->total;
         }
-        $y = ($key ?? 1) + 13;
+        $y = ($key ?? 1) + 5;
         $spreadsheet->getActiveSheet()->setCellValue("A{$y}", 'TOTALES');
         $this->addHeaderStyle($spreadsheet, "A{$y}");
         $spreadsheet->getActiveSheet()->setCellValue("B{$y}", $sumtotal);
         $this->addHeaderStyle($spreadsheet, "B{$y}");
-        $spreadsheet->getActiveSheet()->getStyle("A10:B{$y}")->applyFromArray([
+        $spreadsheet->getActiveSheet()->getStyle("A3:B{$y}")->applyFromArray([
             'borders' => [
                 'allBorders' => [
                     'borderStyle' => \PhpOffice\PhpSpreadsheet\Style\Border::BORDER_THIN,
@@ -482,22 +444,22 @@ class XlssExport
             ],
         ]);
 
-        $spreadsheet->getActiveSheet()->setCellValue('D10', 'OTROS');
-        $this->addHeaderStyle($spreadsheet, 'D10');
-        $spreadsheet->getActiveSheet()->mergeCells('D10:F10', \PhpOffice\PhpSpreadsheet\Worksheet\Worksheet::MERGE_CELL_CONTENT_MERGE);
-        $spreadsheet->getActiveSheet()->setCellValue('D11', 'BAJA ACUMULADA');
-        $this->addHeaderStyle($spreadsheet, 'D11');
-        $spreadsheet->getActiveSheet()->setCellValue('E11', 'OTRAS BIBLIOTECAS');
-        $this->addHeaderStyle($spreadsheet, 'E11');
-        $spreadsheet->getActiveSheet()->setCellValue('F11', 'TOTAL');
-        $this->addHeaderStyle($spreadsheet, 'F11');
-        $spreadsheet->getActiveSheet()->setCellValue('D12', $dataUbicaciones['Baja']->total);
-        $spreadsheet->getActiveSheet()->setCellValue('E12', $dataUbicaciones['otrasBibCount']);
-        $spreadsheet->getActiveSheet()->setCellValue('F12', ((int)$dataUbicaciones['Baja']->total + (int)$dataUbicaciones['otrasBibCount']));
-        $spreadsheet->getActiveSheet()->setCellValue('D13', $dataUbicaciones['Baja']->total);
-        $spreadsheet->getActiveSheet()->setCellValue('E13', $dataUbicaciones['otrasBibCount']);
-        $spreadsheet->getActiveSheet()->setCellValue('F13', ((int)$dataUbicaciones['Baja']->total + (int)$dataUbicaciones['otrasBibCount']));
-        $spreadsheet->getActiveSheet()->getStyle("D10:F13")->applyFromArray([
+        $spreadsheet->getActiveSheet()->setCellValue('D3', 'OTROS');
+        $this->addHeaderStyle($spreadsheet, 'D3');
+        $spreadsheet->getActiveSheet()->mergeCells('D3:F3', \PhpOffice\PhpSpreadsheet\Worksheet\Worksheet::MERGE_CELL_CONTENT_MERGE);
+        $spreadsheet->getActiveSheet()->setCellValue('D4', 'BAJA ACUMULADA');
+        $this->addHeaderStyle($spreadsheet, 'D4');
+        $spreadsheet->getActiveSheet()->setCellValue('E4', 'OTRAS BIBLIOTECAS');
+        $this->addHeaderStyle($spreadsheet, 'E4');
+        $spreadsheet->getActiveSheet()->setCellValue('F4', 'TOTAL');
+        $this->addHeaderStyle($spreadsheet, 'F4');
+        $spreadsheet->getActiveSheet()->setCellValue('D5', $dataUbicaciones['Baja']->total);
+        $spreadsheet->getActiveSheet()->setCellValue('E5', $dataUbicaciones['otrasBibCount']);
+        $spreadsheet->getActiveSheet()->setCellValue('F5', ((int)$dataUbicaciones['Baja']->total + (int)$dataUbicaciones['otrasBibCount']));
+        $spreadsheet->getActiveSheet()->setCellValue('D6', $dataUbicaciones['Baja']->total);
+        $spreadsheet->getActiveSheet()->setCellValue('E6', $dataUbicaciones['otrasBibCount']);
+        $spreadsheet->getActiveSheet()->setCellValue('F6', ((int)$dataUbicaciones['Baja']->total + (int)$dataUbicaciones['otrasBibCount']));
+        $spreadsheet->getActiveSheet()->getStyle("D3:F5")->applyFromArray([
             'borders' => [
                 'allBorders' => [
                     'borderStyle' => \PhpOffice\PhpSpreadsheet\Style\Border::BORDER_THIN,
@@ -606,31 +568,41 @@ class XlssExport
         $spreadsheet->getActiveSheet()->mergeCells("E{$initialMergeUbication}:E$y", \PhpOffice\PhpSpreadsheet\Worksheet\Worksheet::MERGE_CELL_CONTENT_MERGE);
         $spreadsheet->getActiveSheet()->mergeCells("G{$initialMergeUbication}:G$y", \PhpOffice\PhpSpreadsheet\Worksheet\Worksheet::MERGE_CELL_CONTENT_MERGE);
         $spreadsheet->getActiveSheet()->mergeCells("I{$initialMergeUbication}:I$y", \PhpOffice\PhpSpreadsheet\Worksheet\Worksheet::MERGE_CELL_CONTENT_MERGE);
-        dump($consolidado);die;
+
         $y += 1;
         $spreadsheet->getActiveSheet()->setCellValue("A{$y}", 'Faltante');
         $spreadsheet->getActiveSheet()->setCellValue("B{$y}", 'Nivel Central');
         $spreadsheet->getActiveSheet()->mergeCells("B{$y}:C{$y}", \PhpOffice\PhpSpreadsheet\Worksheet\Worksheet::MERGE_CELL_CONTENT_MERGE);
-        $spreadsheet->getActiveSheet()->setCellValue("D{$y}", $totalFaltanteNivelCentral);
-        $spreadsheet->getActiveSheet()->setCellValue("F{$y}", '$' . number_format($consolidadoFaltanteNivelCentralPrecio, 2));
-        $spreadsheet->getActiveSheet()->setCellValue("H{$y}", number_format($porcentajeFaltanteNivelCentral, 2) . ' %');
-
+        $spreadsheet->getActiveSheet()->setCellValue("D{$y}", $consolidado['FaltanteNivelCentral']->total);
+        $spreadsheet->getActiveSheet()->setCellValue("F{$y}", '$' . number_format($consolidado['FaltanteNivelCentral']->totalprice, 2));
+        $spreadsheet->getActiveSheet()->setCellValue(
+            "H{$y}",
+            number_format(
+                $consolidado['FaltanteNivelCentral']->total ? ($consolidado['FaltanteNivelCentral']->total / $consolidado['sumaTotal']->total) * 100 : 0,
+                2
+            ) . ' %' . ' %'
+        );
         $y += 1;
         $spreadsheet->getActiveSheet()->setCellValue("A{$y}", 'Faltante');
-        $spreadsheet->getActiveSheet()->setCellValue("B{$y}", 'Tránsito');
+        $spreadsheet->getActiveSheet()->setCellValue("B{$y}", 'En Tránsito');
         $spreadsheet->getActiveSheet()->mergeCells("B{$y}:C{$y}", \PhpOffice\PhpSpreadsheet\Worksheet\Worksheet::MERGE_CELL_CONTENT_MERGE);
-        $spreadsheet->getActiveSheet()->setCellValue("D{$y}", $totalFaltanteTransito);
-        $spreadsheet->getActiveSheet()->setCellValue("F{$y}", '$' . number_format($consolidadoFaltanteTransitoPrecio, 2));
-        $spreadsheet->getActiveSheet()->setCellValue("H{$y}", number_format($porcentajeFaltanteTransito, 2) . ' %');
-
+        $spreadsheet->getActiveSheet()->setCellValue("D{$y}", $consolidado['FaltanteTransito']->total);
+        $spreadsheet->getActiveSheet()->setCellValue("F{$y}", '$' . number_format($consolidado['FaltanteTransito']->totalprice, 2));
+        $spreadsheet->getActiveSheet()->setCellValue(
+            "H{$y}",
+            number_format(
+                $consolidado['FaltanteTransito']->total ? ($consolidado['FaltanteTransito']->total / $consolidado['sumaTotal']->total) * 100 : 0,
+                2
+            ) . ' %' . ' %'
+        );
         $y += 1;
         $spreadsheet->getActiveSheet()->setCellValue("A{$y}", 'Colección Total');
         $spreadsheet->getActiveSheet()->mergeCells("A{$y}:C{$y}", \PhpOffice\PhpSpreadsheet\Worksheet\Worksheet::MERGE_CELL_CONTENT_MERGE);
         $this->addHeaderStyle($spreadsheet, "A{$y}:C{$y}");
-        $spreadsheet->getActiveSheet()->setCellValue("D{$y}", $totalSuma);
+        $spreadsheet->getActiveSheet()->setCellValue("D{$y}", $consolidado['sumaTotal']->total);
         $spreadsheet->getActiveSheet()->mergeCells("D{$y}:E{$y}", \PhpOffice\PhpSpreadsheet\Worksheet\Worksheet::MERGE_CELL_CONTENT_MERGE);
         $this->addHeaderStyle($spreadsheet, "D{$y}:E{$y}");
-        $spreadsheet->getActiveSheet()->setCellValue("F{$y}", '$' . number_format($totalSumaPrecio, 2));
+        $spreadsheet->getActiveSheet()->setCellValue("F{$y}", '$' . number_format($consolidado['sumaTotal']->totalprice, 2));
         $spreadsheet->getActiveSheet()->mergeCells("F{$y}:G{$y}", \PhpOffice\PhpSpreadsheet\Worksheet\Worksheet::MERGE_CELL_CONTENT_MERGE);
         $this->addHeaderStyle($spreadsheet, "F{$y}:G{$y}");
         $spreadsheet->getActiveSheet()->setCellValue("H{$y}", '100 %');
